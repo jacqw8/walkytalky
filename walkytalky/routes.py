e398e34f33ba966b1ac11d2dde59ed49814d72a5
jacqw8 <jacquelinewu88@gmail.com>
2021-03-13 16:21:24 -0600

    forms

--- walkytalky/routes.py
+++ walkytalky/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request, send_from_directory, Response
 from walkytalky import app
+from walkytalky.forms import RegistrationForm, LoginForm
 
 
