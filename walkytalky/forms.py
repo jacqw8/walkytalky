263e81df3c4d851b15e2a9f6ef0d752e2ba01a06
jacqw8 <jacquelinewu88@gmail.com>
2021-03-13 19:10:31 -0600

    import user

--- walkytalky/forms.py
+++ walkytalky/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
+from walkytalky.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
