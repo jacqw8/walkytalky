dc9dda72e08bf71ef6f27d37833b0c48bb871f62
jacqw8 <jacquelinewu88@gmail.com>
2021-03-14 11:57:37 -0500

    only you read

--- walkytalky/routes.py
+++ walkytalky/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request, send_from_directory, Response
+from flask import render_template, url_for, flash, redirect, request, send_from_directory, Response, jsonify, abort
 from walkytalky import app, db, bcrypt
 from walkytalky.forms import RegistrationForm, LoginForm, CalendarForm, CalendarForm2, PostForm, SearchFriend
 from flask_login import login_user, current_user, logout_user, login_required
@@ -63,30 +63,48 @@ def updateavail():
 @login_required
 def times():
     posts = Post.query.all()
+    for post in posts:
+        if post.author != current_user:
+            abort(403)
     return render_template('times.html', posts=posts)
 
 @app.route("/post/<int:post_id>")
 @login_required
 def post(post_id):
     post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
     return render_template('post.html', title=post.title, post=post)
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
     db.session.delete(post)
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('times'))
 
-@app.route("/friends")
-@login_required
-def friends():
-    form = SearchFriend()
-    if form.validate_on_submit():
-        friends = form.search.data
-        return render_template('friends.html', friends=friends)
-    return render_template('search.html', form=form)
+# @app.route("/friends")
+# @login_required
+# def friends():
+#     form = SearchFriend()
+#     if form.validate_on_submit():
+#         friends = form.search.data
+#         return render_template('friends.html', friends=friends)
+#     return render_template('search.html', form=form)
+
+all_users = []
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = db.session.query(User).all()
+    all_users = jsonify(users=[i.serialize for i in users])
+    return all_users
+
+# @app.route('/post_user', methods=['POST'])
+# def post_user():
+#     data = request.get_json()
+#     user = User(data['username'], data['email'])
+#     db.session.add(user)
+#     db.session.commit()
+#     return jsonify(**data)
