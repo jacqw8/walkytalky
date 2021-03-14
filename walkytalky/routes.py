51046341a056383bcea60b85a0d94ad5c1b55759
jacqw8 <jacquelinewu88@gmail.com>
2021-03-14 11:17:10 -0500

    add and delete times

--- walkytalky/routes.py
+++ walkytalky/routes.py
@@ -89,6 +89,24 @@ def updateavail():
     return render_template('updateavail.html', form=form)
 
 @app.route("/times")
+@login_required
 def times():
     posts = Post.query.all()
     return render_template('times.html', posts=posts)
+
+@app.route("/post/<int:post_id>")
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('times'))
